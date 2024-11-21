52412f5057c9df39c2a41feb3d8fc1150bcc0caa
msa507 <msa10101962@gmail.com>
2024-11-21 14:14:07 +0500

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -2,11 +2,10 @@ class House:
     def __init__(self, name, number_of_floors):
          self.name = name
          self.number_of_floors = number_of_floors
-         # return (self.number_of_floors)
 
     def go_to(self, floor):
         from time import sleep
-        if floor < self.number_of_floors or floor < 1:
+        if floor < self.number_of_floors and floor > 0:
             for num in range(1, floor + 1):
                 sleep(0.5)
                 print(num)
@@ -19,3 +18,4 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+
