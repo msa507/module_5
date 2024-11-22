f5f15e4dbb2ae94151824aacb63bd5ae36acc664
msa507 <msa10101962@gmail.com>
2024-11-22 12:12:23 +0500

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,66 @@
+class House:
+    def __init__(self, name, number_of_floors):
+         self.name = name
+         self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        """Вывод в консоль"""
+        print(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 != h2)
+
