ee8553a1f5c7f986093c4e625aee9e8262f60348
msa507 <msa10101962@gmail.com>
2024-12-06 10:53:33 +0500

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,41 +1,42 @@
 class User:
-
-
-    def __init__(self, nickname, password, age):
+    def __init__(self, nickname, hashpass, age):
         self.nickname = nickname
-        self.password = password
+        self.password = hashpass
         self.age = age
 
-    def __hash__(self):
-        return hash(self.password)
 
 class Video:
-
-    def __init__(self, title, duration):
+    def __init__(self, title, duration, adult_mode = False):
         self.title = title
         self.duration = duration
         self.time_now = 0
-        self.adult_mode = False
+        self.adult_mode = adult_mode
+        self.users = []
+        self.current_user = None
+
 
 class UrTube(User, Video):
+    def log_in(self, login, password):
+        hashpass = hash(password)
+        for user in self.users:
+            if (user.nickname == login) and (user.password == hashpass):
+                self.current_user = user
 
-    def log_in(self, nickname, password):
-        self.nickname = nickname
-        self.password = password
-        if self.nickname == User.__init__(nickname):
-            print('Есть такой пользователь')
-        else: print('Нет такого пользователя')
+    def register(self, nickname, hashpass, age):
+        new_user = User(nickname, hashpass, age)
+        self.users.append(new_user)
+        self.current_user = new_user
 
     def log_out(self):
-        self.user = None
-
-
-
-
-us1 = User('Serg', '258', 62)
-
+        self.user_user = None
 
 
 
 # v1 = Video('Лучший язык программирования 2024 года', 200)
-# print(v1.title, v1.duration, v1.time_now, v1.adult_mode)
+# v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+
+
+
+
+
